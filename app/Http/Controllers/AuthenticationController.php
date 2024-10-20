999273e1dab28671405b9096331d4556f1de7529
Soeng Souy <soengsouy.khmer09@gmail.com>
2024-10-20 09:16:40 +0700

    update login

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Session;
+use Auth;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use Session;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     /** home page dashboard */
-    public function index()
+    public function index(Request $request)
     {
-        return view('dashboard.home');
+        return view('dashboard.home'); // Redirect to the home page
     }
 }
