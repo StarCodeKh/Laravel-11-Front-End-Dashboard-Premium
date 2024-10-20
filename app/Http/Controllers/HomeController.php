a9dc04f6ebd9466f916679ccfcce768657b7b1ac
Soeng Souy <soengsouy.khmer09@gmail.com>
2024-10-20 09:46:15 +0700

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,6 +10,11 @@ class HomeController extends Controller
     /** home page dashboard */
     public function index(Request $request)
     {
-        return view('dashboard.home'); // Redirect to the home page
+        if ($request->session()->has('token')) {
+            return view('dashboard.home'); // Redirect to the home page
+        } else {
+            flash()->error('You are not logged in.'); // Flash an error message if the session is not active
+            return redirect()->route('login'); // Redirect to the login page if the session is not active
+        }
     }
 }
